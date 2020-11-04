17ce4188202b87a525a06cc1c4e27689b5a879da
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-11-04 14:08:33 -0500

    hero-child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroChildComponent } from './hero-child/hero-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroChildComponent
   ],
   imports: [
     BrowserModule,
