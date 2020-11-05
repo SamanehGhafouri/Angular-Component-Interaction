5817990e5c173d7ccc99da6e96c8738c39ad4d71
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-11-04 21:13:05 -0500

    intercept input property changes with a setter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroChildComponent } from './hero-child/hero-child.component';
 import { HeroParentComponent } from './hero-parent/hero-parent.component';
+import { NameChildComponent } from './name-child/name-child.component';
+import { NameParentComponent } from './name-parent/name-parent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroChildComponent,
-    HeroParentComponent
+    HeroParentComponent,
+    NameChildComponent,
+    NameParentComponent
   ],
   imports: [
     BrowserModule,
